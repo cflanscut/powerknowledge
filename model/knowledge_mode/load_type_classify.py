f5e16e110070ed3a932ddf38e35d3beeef801ace
Lan Chaofan <cflanscut@163.com>
2021-03-02 15:21:04 +0800

    增加混淆矩阵

--- model/knowledge_mode/load_type_classify.py
+++ model/knowledge_mode/load_type_classify.py
@@ -10,9 +10,21 @@ sys.path.append(r'model/')
 from classifieddata import read_processed_data
 from linear import SingleLayerModel, DoubleLayerModel, MultiLayerModer
 
+
+def confusion_matrix(preds, labels, conf_matrix):
+    for p, l in zip(preds, labels):
+        conf_matrix[p, l] += 1
+    return conf_matrix
+
+
 process_start_time = time.time()
-label_transformer = {'I': 0, 'R': 1, 'NL': 2}
-x, y = read_processed_data('load', Transformer=label_transformer)
+label_transformer = {'I': 0, 'R': 1, 'NL': 0}
+feature_select = ['P_F', 'z_hp1']
+x, y = read_processed_data('load',
+                           feature_select=feature_select,
+                           direaction=1,
+                           offset=30,
+                           Transformer=label_transformer)
 print('finished reading data, cost %2.2f s' %
       (time.time() - process_start_time))
 x_mean = np.mean(x, axis=0)
@@ -21,13 +33,6 @@ for i in range(len(x)):
     for j in range(len(x[0])):
         if x_std[j] != 0:
             x[i][j] = (x[i][j] - x_mean[j]) / x_std[j]
-# for i, label in enumerate(y):
-#     if label == 'I':
-#         y[i] = '0'
-#     elif label == 'R':
-#         y[i] = '1'
-#     elif label == 'NL':
-#         y[i] = '2'
 
 x_train = torch.tensor(x).float()
 y_train = torch.tensor(y.astype(float)).float()
@@ -35,16 +40,9 @@ y_train = torch.tensor(y.astype(float)).float()
 dataset = Data.TensorDataset(x_train, y_train)
 dataloader = Data.DataLoader(dataset=dataset, batch_size=128, shuffle=True)
 
-# def get_acc(outputs, labels):
-#     counts = 0
-#     for index, output in enumerate(outputs):
-#         if torch.argmax(output) == labels[index]:
-#             counts += 1
-#     return counts / len(labels)
-
-# model = SingleLayerModel(x_train.size()[1], 3)
-model = DoubleLayerModel(x_train.size()[1], 30, 3)
-# model = MultiLayerModer(x_train.size()[1], 3)
+model = SingleLayerModel(x_train.size()[1], 2)
+# model = DoubleLayerModel(x_train.size()[1], 3, 2)
+# model = MultiLayerModer(x_train.size()[1], 2)
 optim = torch.optim.SGD(model.parameters(), lr=0.05)
 loss_f = nn.CrossEntropyLoss()
 epoch_num = 200
@@ -69,10 +67,27 @@ for epoch in range(epoch_num):
            epoch_acc / y.__len__(), epoch_loss / y.__len__()))
 
 test_predict = model(x_train)
-test_label = np.argmax(test_predict.data.numpy(), axis=1)
+test_predict = np.argmax(test_predict.data.numpy(), axis=1)
 test_result = pd.concat([
-    pd.DataFrame({'predict': test_label}),
+    pd.DataFrame({'predict': test_predict}),
     pd.DataFrame({'label': y_train.numpy()})
 ],
                         axis=1)
-test_result.to_csv('model/knowledge_mode/load_test_result.csv', index=True, sep=',')
+conf_matrix = np.zeros([2, 2])
+conf_matrix = confusion_matrix(test_predict,
+                               y_train.numpy().astype(int),
+                               conf_matrix=conf_matrix)
+conf_matrix = conf_matrix.astype(int)
+test_result = pd.concat([
+    test_result,
+    pd.DataFrame({
+        'label': ['I/NL', 'R'],
+        'I/NL': conf_matrix[0, :],
+        'R': conf_matrix[1, :],
+        # 'NL': conf_matrix[2, :]
+    })
+],
+                        axis=1)
+test_result.to_csv('model/knowledge_mode/load_test_result.csv',
+                   index=True,
+                   sep=',')
