6f3065390f1ffcd6b542d9b161ab2e5172f7efc5
Lan Chaofan <cflanscut@163.com>
2021-03-07 15:46:10 +0800

    判断外特性（nn和决策树）

---
+++ model/knowledge_mode/loadtype/read_load_data.py
@@ -0,0 +1,17 @@
+import numpy as np
+import time
+import sys
+sys.path.append(r'data/')
+from read_PLAID_data import read_processed_data
+
+process_start_time = time.time()
+label_transformer = {'I': 0, 'R': 1, 'NL': 0}
+x, y = read_processed_data('load',
+                           direaction=1,
+                           offset=30,
+                           Transformer=label_transformer)
+print('finished reading data, cost %2.2f s' %
+      (time.time() - process_start_time))
+x = x[:, 1:]
+np.savetxt('model/knowledge_mode/loadtype/y_label.csv', y, delimiter=',')
+np.savetxt('model/knowledge_mode/loadtype/x.csv', x, delimiter=',')
