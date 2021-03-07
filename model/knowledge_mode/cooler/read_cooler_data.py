d6dbb1403c25be61963d16059a5bf75581b6c7e0
Lan Chaofan <cflanscut@163.com>
2021-03-07 15:44:52 +0800

    判断制冷决策树

---
+++ model/knowledge_mode/cooler/read_cooler_data.py
@@ -0,0 +1,18 @@
+import numpy as np
+import time
+import sys
+sys.path.append(r'data/')
+from read_PLAID_data import read_processed_data
+
+process_start_time = time.time()
+label_transformer = {'0': 0, '1': 1}
+x, y = read_processed_data('is_cool',
+                           type_header='extra label',
+                           direaction=1,
+                           offset=30,
+                           Transformer=label_transformer)
+print('finished reading data, cost %2.2f s' %
+      (time.time() - process_start_time))
+x = x[:, 1:]
+np.savetxt('model/knowledge_mode/cooler/y_label.csv', y, delimiter=',')
+np.savetxt('model/knowledge_mode/cooler/x.csv', x, delimiter=',')
