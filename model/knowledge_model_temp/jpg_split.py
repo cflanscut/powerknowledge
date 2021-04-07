a6546b857e6551e783ae46315e260217a97a8766
Lan Chaofan <cflanscut@163.com>
2021-04-07 09:19:14 +0800

    划分图片自动生成标签目录

--- model/knowledge_model_temp/jpg_split.py
+++ model/knowledge_model_temp/jpg_split.py
@@ -18,15 +18,24 @@ def move_file(file, old_path, new_path):
 
 
 type_index = read_index('type')
-path = 'model/knowledge_model_temp/jpg'
+path = 'model/knowledge_model_temp/diff_jpg/'
+if not os.path.exists(path + 'total'):
+    os.mkdir(path + 'total')
+file_list = os.listdir(path)
+for file in file_list:
+    if '.jpg' not in file:
+        continue
+    move_file(file, path, path + 'total')
 
 for key in type_index:
     each_list = type_index[key]
+    if not os.path.exists(path + key):
+        os.mkdir(path + key)
     for i, index in enumerate(each_list):
         file_name = str(index) + '.jpg'
-        jpg_list = os.listdir(path + '/total')
+        jpg_list = os.listdir(path + 'total')
         if file_name not in jpg_list:
             continue
-        copy_file(file_name, path + '/total', path + '/' + key)
+        copy_file(file_name, path + 'total', path + key)
 
 print('finished spliting!')
