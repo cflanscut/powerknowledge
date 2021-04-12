49fb887caf15e30a5f70dc4fac00e4ed49b21d7d
Lan Chaofan <cflanscut@163.com>
2021-04-12 19:35:45 +0800

    增加单个设备波形示例

--- data/source/slider_plot.py
+++ data/source/slider_plot.py
@@ -6,7 +6,17 @@ from read_PLAID_data import read_source_data, slider_plot
 
 source_dir = 'data/source/submetered_new_pured/source/'
 file_list = os.listdir(source_dir)
-
+# with open('model/find_result.txt', 'r') as f:
+#     file_list = []
+#     for line in f.readlines():
+#         line = line.strip('\n')
+#         line = line.split(':')
+#         file = line[0][1:-1]
+#         if '.csv' in file:
+#             file_list.append(file)
+file_dir = source_dir + '1670.csv'
+Switch_V, Switch_I = read_source_data(file_dir)
+slider_plot(Switch_I)
 with open(
         '/home/chaofan/powerknowledge/data/source/metadata_submetered2.1.json',
         'r',
